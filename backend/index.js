f903ee92b83c9088af9b0f14180520b480173bbb
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-08-31 22:52:46 +0530

    done cors part 1

--- backend/index.js
+++ backend/index.js
@@ -6,23 +6,30 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// const allowedOrigins = [
+//     'http://localhost:3000', // Replace with your local frontend address
+//     // 'https://your-vercel-app-name.vercel.app', // Replace with your Vercel deployment URL
+//     'https://text-util-rosy.vercel.app/'
+//   ];
+  
+//   const corsOptions = {
+//     origin: function (origin, callback) {
+//       if (allowedOrigins.includes(origin) || !origin) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error('Not allowed by CORS'));
+//       }
+//     },
+//   };
+  
+//   app.use(cors(corsOptions));
 const allowedOrigins = [
-    'http://localhost:3000', // Replace with your local frontend address
-    // 'https://your-vercel-app-name.vercel.app', // Replace with your Vercel deployment URL
-    'https://text-util-rosy.vercel.app/'
+    'https://text-util-rosy.vercel.app', // Replace with your Vercel frontend URL
   ];
   
-  const corsOptions = {
-    origin: function (origin, callback) {
-      if (allowedOrigins.includes(origin) || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-  };
-  
-  app.use(cors(corsOptions));
+  app.use(cors({
+    origin: allowedOrigins,
+  }));
 // app.use(cors());
 app.use(express.json());
 
