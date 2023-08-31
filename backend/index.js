cba1af864422fa6aa1da5252a792ff5b000ca172
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-08-31 22:41:57 +0530

    uodate the cors

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,24 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+const allowedOrigins = [
+    'http://localhost:3000', // Replace with your local frontend address
+    // 'https://your-vercel-app-name.vercel.app', // Replace with your Vercel deployment URL
+    'https://text-util-rosy.vercel.app/'
+  ];
+  
+  const corsOptions = {
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+  };
+  
+  app.use(cors(corsOptions));
+// app.use(cors());
 app.use(express.json());
 
 // Define database and collection names
