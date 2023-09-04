963c0deb50c32fe1fcc4701ced6b60674c6e299d
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-09-04 08:41:27 +0530

    push the code

--- src/components/news.js
+++ src/components/news.js
@@ -8,7 +8,17 @@ function News(props) {
     const [loading, setLoading] = useState(false);
     const [page, setPage] = useState(1);
     const [totalResults, setTotalResults] = useState(0);
+    const [searchTerm, setSearchTerm] = useState('');
 
+    const handleSearch = () => {
+        // Perform the search based on the source name
+        // You can filter the articles based on the source name and update the state
+        const filteredArticles = articles.filter((element) =>
+          element.source.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setArticles(filteredArticles);
+      };
+      
     const updateNews = async (pageNo) => {
         // const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apikey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
         const url=`https://gnews.io/api/v4/top-headlines?country=${props.country}&category=${props.category}&apikey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
@@ -18,6 +28,9 @@ function News(props) {
         setArticles(parsedData.articles);
         setTotalResults(parsedData.totalResults);
         setLoading(false);
+
+        // Reset the search term when new data is fetched
+        setSearchTerm('');
     }
 
     useEffect(() => {
@@ -39,6 +52,19 @@ function News(props) {
 
     return (
         <div className='container my-3 mr-3' style={mystyle}>
+        <div className="container d-flex justify-content-between">
+                <input
+                    type="text"
+                    className='mx-4 form-control'
+                    placeholder="Search by source name..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button type="button" className="btn btn-primary" onClick={handleSearch}>
+                    Search
+                </button>
+        </div>
+
             <h1 className='text-center'>This is {props.heading} Page - Top Headlines!</h1>
             <div className="container d-flex justify-content-between">
                 <button disabled={page <= 1} type="button" className="btn btn-dark" onClick={handlePrevClick}>
@@ -49,7 +75,7 @@ function News(props) {
                 </button>
             </div>
             {loading && <Loader />}
-            <div className="row mx-5">
+            <div className="row mx-auto">
                 {!loading && articles.map((element) => (
                     <div className="col-md-4" key={element.url}>
                         <NewsItem
