e322fcad410e9a1fcaae1e84c77523797e32a76d
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-09-07 21:18:54 +0530

    done cors

--- apitest/api/app.py
+++ apitest/api/app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
 from gensim.summarization import summarize
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
+
 
 @app.route('/')
 def hello_world():
