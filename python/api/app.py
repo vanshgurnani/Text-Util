898f11a792d9c9ce9fe9cb8626b4c3a623d823ec
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-09-07 16:47:24 +0530

    done nltk work

--- python/api/app.py
+++ python/api/app.py
@@ -1,51 +1,51 @@
 from flask import Flask, request, jsonify
-# import nltk
-# from nltk.chat.util import Chat, reflections
+import nltk
+from nltk.chat.util import Chat, reflections
 
 app = Flask(__name__)
 
-# # Define the chatbot responses
-# pairs = [
-#     [
-#         r"my name is (.*)",
-#         ["Hello %1, how can I help you today?",]
-#     ],
-#     [
-#         r"what is your name?",
-#         ["I am just a chatbot created with NLTK.",]
-#     ],
-#     [
-#         r"how are you?",
-#         ["I'm doing good. How about you?",]
-#     ],
-#     [
-#         r"sorry (.*)",
-#         ["No problem, I understand.",]
-#     ],
-#     [
-#         r"(.*) (good|great)",
-#         ["That's good to hear.",]
-#     ],
-#     [
-#         r"bye",
-#         ["Goodbye! Have a nice day.",]
-#     ],
-# ]
+# Define the chatbot responses
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, how can I help you today?",]
+    ],
+    [
+        r"what is your name?",
+        ["I am just a chatbot created with NLTK.",]
+    ],
+    [
+        r"how are you?",
+        ["I'm doing good. How about you?",]
+    ],
+    [
+        r"sorry (.*)",
+        ["No problem, I understand.",]
+    ],
+    [
+        r"(.*) (good|great)",
+        ["That's good to hear.",]
+    ],
+    [
+        r"bye",
+        ["Goodbye! Have a nice day.",]
+    ],
+]
 
 # Create a chatbot instance
-# chatbot = Chat(pairs, reflections)
+chatbot = Chat(pairs, reflections)
 
 # Define a route for the root directory ("/")
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-# @app.route('/get_response', methods=['POST'])
-# def get_response():
-#     user_message = request.json.get('message')
-#     response = chatbot.respond(user_message)
-#     return jsonify({"response": response})
+@app.route('/get_response', methods=['POST'])
+def get_response():
+    user_message = request.json.get('message')
+    response = chatbot.respond(user_message)
+    return jsonify({"response": response})
 
 if __name__ == '__main__':
-    # nltk.download('punkt')  # Download NLTK data (if not already downloaded)
+    nltk.download('punkt')  # Download NLTK data (if not already downloaded)
     app.run(debug=True)
