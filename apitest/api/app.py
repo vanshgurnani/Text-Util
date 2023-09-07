f94c3ee64774ed1d1b8f77e2545c2a78a62bd92e
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-09-07 21:16:36 +0530

    done api text summeary

--- apitest/api/app.py
+++ apitest/api/app.py
@@ -1,55 +1,21 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
-import nltk
-from nltk.chat.util import Chat, reflections
+from gensim.summarization import summarize
 
 app = Flask(__name__)
 
-CORS(app)  # Enable CORS for your Flask app
-
-# Define the chatbot responses
-pairs = [
-    [
-        r"my name is (.*)",
-        ["Hello %1, how can I help you today?",]
-    ],
-    [
-        r"what is your name?",
-        ["I am just a chatbot created with NLTK.",]
-    ],
-    [
-        r"how are you?",
-        ["I'm doing good. How about you?",]
-    ],
-    [
-        r"sorry (.*)",
-        ["No problem, I understand.",]
-    ],
-    [
-        r"(.*) (good|great)",
-        ["That's good to hear.",]
-    ],
-    [
-        r"bye",
-        ["Goodbye! Have a nice day.",]
-    ],
-]
-
-# Create a chatbot instance
-chatbot = Chat(pairs, reflections)
-
-# Define a route for the root directory ("/")
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-# Define the /get_response endpoint to accept both GET and POST requests
-@app.route('/get_response', methods=['POST'])
-def get_response():
-    user_message = request.json.get('message')
-    response = chatbot.respond(user_message)
-    return jsonify({"response": response})
+@app.route('/get_summary', methods=['POST'])
+def get_summary():
+    data = request.json
+    text = data["text"]
+
+    # Perform extractive summarization using TextRank
+    summary = summarize(text)
+
+    return jsonify({"summary": summary})
 
 if __name__ == '__main__':
-    nltk.download('punkt')  # Download NLTK data (if not already downloaded)
     app.run(debug=True)
--- src/components/chat.jsx
+++ src/components/chat.jsx
@@ -1,11 +1,10 @@
-// src/Chat.js
-
 import React, { useState } from 'react';
 import axios from 'axios';
 
 const Chat = () => {
   const [input, setInput] = useState('');
   const [messages, setMessages] = useState([]);
+  const [summary, setSummary] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -16,23 +15,24 @@ const Chat = () => {
 
     // Send the user message to the server for a response
     try {
-      const response = await axios.post('https://text-util-c6tr.vercel.app/get_response', { message: input });
-      const botResponse = response.data.response;
+      const response = await axios.post('https://text-util-c6tr.vercel.app/get_summary', { text: input });
+      const botResponse = response.data.summary;
 
-      // Add bot response to the chat
+      // Add bot response (summary) to the chat
       setMessages([...messages, { text: botResponse, user: 'bot' }]);
+      setSummary(botResponse); // Update the summary state
     } catch (error) {
       console.error('Error sending message:', error);
     }
   };
 
   const handleClear = () => {
-    // Clear the chat messages
+    // Clear the chat messages and summary
     setMessages([]);
+    setSummary('');
   };
 
   return (
-    <>
     <div className='d-flex justify-content-center'>
       <form method='post' onSubmit={handleSubmit}>
         <input
@@ -46,8 +46,11 @@ const Chat = () => {
           Clear
         </button>
       </form>
-    </div>
-    <div className='d-flex justify-content-center'>
+      <div className="summary">
+        <h3>Summary:</h3>
+        <p>{summary}</p>
+      </div>
+      <div className="chat">
         {messages.map((message, index) => (
           <div
             key={index}
@@ -57,7 +60,7 @@ const Chat = () => {
           </div>
         ))}
       </div>
-    </>
+    </div>
   );
 };
 
