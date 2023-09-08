48dcf6bc59b587474a9e42e0d4f43a9d40bba522
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-09-08 22:33:20 +0530

    done

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import Spinner from './Spinner'
+import Spinner from './spinner'
 
 function App(props) {
   const [text, setText] = useState('');
