9d417545267bd7f2a0cccd2ee5f5e854217bd1c7
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-09-08 20:40:39 +0530

    done python

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const { exec } = require('child_process');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -49,7 +50,17 @@ const NoteSchema = new mongoose.Schema({
 const Note = mongoose.model(COLLECTION_NAME, NoteSchema);
 
 
-
+app.get('/run-python', (req, res) => {
+  // Run the Python script as a child process
+  exec('app.py', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing Python script: ${error}`);
+      return res.status(500).send('Internal Server Error');
+    }
+    console.log(`Python script output: ${stdout}`);
+    res.send('Python script executed successfully');
+  });
+});
 
 
 // Define a route for the home route ("/")
