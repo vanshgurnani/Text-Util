3108350925ab07065a04e1c9bc47677ce829ceed
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-09-09 13:54:16 +0530

    done

--- backend/notes/userModel.js
+++ backend/notes/userModel.js
@@ -6,6 +6,6 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
 });
 
-const User = mongoose.model('user-data', userSchema);
+const User = mongoose.model('user', userSchema);
 
 module.exports = User;
--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,45 +1,11 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react'
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [alert, setAlert] = useState({ type: '', message: '' });
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await axios.post('https://text-util-83cs.vercel.app/api/login', { email, password });
-      // Handle successful login, e.g., store the token in localStorage and redirect
-    } catch (error) {
-      console.error('Error logging in:', error);
-      // Handle login error
-      setAlert({ type: 'error', message: 'Invalid email or password' });
-    }
-  };
-
+function login() {
   return (
-    <div className="container">
-      <h2>Login</h2>
-      {alert.type && (
-        <div className={`alert alert-${alert.type}`} role="alert">
-          {alert.message}
-        </div>
-      )}
-      <form onSubmit={handleSubmit}>
-        <div className="form-group">
-          <label>Email</label>
-          <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </div>
-        <div className="form-group">
-          <label>Password</label>
-          <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </div>
-        <button type="submit" className="btn btn-primary">Login</button>
-      </form>
+    <div>
+      
     </div>
-  );
-};
+  )
+}
 
-export default Login;
+export default login
