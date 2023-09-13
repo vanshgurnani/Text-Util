cd6a78ef3184cd8a5f4db47fdec9b0fdabdc589f
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-09-13 12:46:13 +0530

    summary accuracy partially done

--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -5,6 +5,7 @@ import Spinner from './spinner'
 function App(props) {
   const [text, setText] = useState('');
   const [summary, setSummary] = useState('');
+  const [accuracy, setAccuracy] = useState(null); // Initialize accuracy as null
   const [isLoading, setIsLoading] = useState(false);
 
   const handleTextChange = (e) => {
@@ -12,56 +13,60 @@ function App(props) {
   };
 
   const summarizeText = () => {
-
     setIsLoading(true);
-    // Send a POST request to your Flask API
+
     axios.post('https://flask-production-71e4.up.railway.app/get_summary', { text })
       .then((response) => {
         setSummary(response.data.summary);
+        setAccuracy(response.data.accuracy); // Set accuracy from the API response
       })
       .catch((error) => {
         console.error('Error:', error);
       })
-      .finally(()=>{
+      .finally(() => {
         setIsLoading(false);
       });
   };
 
   const clearText = () => {
-    // Clear the text and summary states
     setText('');
     setSummary('');
+    setAccuracy(null); // Clear accuracy when clearing text
   };
 
   return (
     <div className='container'>
-    <h1 style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>Text Summarizer</h1>
-    <textarea
-      className="form-control"
-      placeholder="Enter text to summarize..."
-      rows="8"
-      cols="20"
-      style={{ resize: 'none' }}
-      value={text}
-      onChange={handleTextChange}
-    ></textarea>
-    <br />
-    <div className="d-flex">
+      <h1 style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>Text Summarizer</h1>
+      <textarea
+        className="form-control"
+        placeholder="Enter text to summarize..."
+        rows="8"
+        cols="20"
+        style={{ resize: 'none' }}
+        value={text}
+        onChange={handleTextChange}
+      ></textarea>
+      <br />
+      <div className="d-flex">
         <button className='btn btn-primary mx-3' onClick={summarizeText}>Summarize</button>
         <button className='btn btn-danger mx-3' onClick={clearText}>Clear</button>
-    </div>
-    {isLoading ? (
-      <Spinner />
-    ) : (
-      summary && (
-        <div className="mt-5 summary-container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
-          <h2>Summary:</h2>
-          <p>{summary}</p>
-        </div>
-      )
-    )}
-  
-
+      </div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        summary && (
+          <div className="mt-5 summary-container" style={{ color: props.mode === 'dark' ? 'white' : '#042743' }}>
+            <h2>Summary:</h2>
+            <p>{summary}</p>
+            {accuracy !== null && ( // Display accuracy only when it's available
+              <div>
+                <h2>Accuracy:</h2>
+                <p>{(accuracy * 100).toFixed(2)}%</p> {/* Format accuracy as a percentage */}
+              </div>
+            )}
+          </div>
+        )
+      )}
     </div>
   );
 }
