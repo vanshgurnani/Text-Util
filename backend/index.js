9634aa95cd7dfa20df29045c1f34ffb7029ec477
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-09-26 23:56:50 +0530

    url updated
    https://text-util-dj8q.vercel.app/

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const allowedOrigins = [
     'https://notenews.onrender.com',
     'https://flask-production-71e4.up.railway.app',
     'https://text-util-five.vercel.app',
-    'https://text-util-i235.vercel.app'
+    'https://text-util-dj8q.vercel.app'
     // 'https://python-jugp.vercel.app',
   // Replace with your Vercel frontend URL
   ];
