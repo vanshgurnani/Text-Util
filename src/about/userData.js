ecddfa36835c67ff92583a9a1d34e590c35d8d28
Vansh Gurnani <gurnanivansh57@gmail.com>
2023-12-04 21:04:27 +0530

    done team members

--- src/about/userData.js
+++ src/about/userData.js
@@ -3,29 +3,29 @@ const userData = [
     { 
       name: 'Vansh Gurnani', 
       additionalInfo: 'Additional info for Vansh.',
-      skills: ['HTML', 'Python', 'Java'],
-      designation: 'Engineer',
+      skills: ['HTML/CSS', 'MERN Stack','Bootstrap','Flask'],
+      designation: 'Full Stack Developer',
       profile:'images/Vansh.jpg'
     },
     { 
       name: 'Yuvraj Singh Lamba', 
       additionalInfo: 'Additional info for Yuvraj.',
-      skills: ['HTML', 'React', 'Java', 'Node.js'],
-      designation: 'Developer',
-      profile:'images/Vansh.jpg'
+      skills: ['HTML/CSS', 'React.js','Bootstrap'],
+      designation: 'Frontend Developer',
+      profile:'images/Yuvraj.jpeg'
     },
     { 
       name: 'Jay Mehta', 
       additionalInfo: 'Additional info for Jay.',
-      skills: ['React', 'HTML', 'Node.js'],
-      designation: 'Analyst',
-      profile:'images/Vansh.jpg'
+      skills: ['HTML/CSS', 'React.js','Bootstrap'],
+      designation: 'Frontend Developer',
+      profile:'images/Jay.jpeg'
     },
     { 
       name: 'Keshav Jha', 
       additionalInfo: 'Additional info for Keshav.',
-      skills: ['Java', 'Python'],
-      designation: 'Engineer',
+      skills: ['HTML/CSS', 'React.js','Bootstrap'],
+      designation: 'Frontend Developer',
       profile:'images/Vansh.jpg'
     },
   ];
