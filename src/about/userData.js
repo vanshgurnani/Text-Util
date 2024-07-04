096ef1963719e2e2b66d89d7bed5f35b18069a61
vanshgurnani <108427430+vanshgurnani@users.noreply.github.com>
2024-07-04 15:52:18 +0530

    Update userData.js

--- src/about/userData.js
+++ src/about/userData.js
@@ -6,28 +6,28 @@ const userData = [
       skills: ['HTML/CSS', 'MERN Stack','Bootstrap','Flask'],
       designation: 'Full Stack Developer',
       profile:'images/Vansh.jpg'
-    },
-    { 
-      name: 'Yuvraj Singh Lamba', 
-      additionalInfo: 'Additional info for Yuvraj.',
-      skills: ['HTML/CSS', 'React.js','Bootstrap'],
-      designation: 'Frontend Developer',
-      profile:'images/Yuvraj.jpeg'
-    },
-    { 
-      name: 'Jay Mehta', 
-      additionalInfo: 'Additional info for Jay.',
-      skills: ['HTML/CSS', 'React.js','Bootstrap'],
-      designation: 'Frontend Developer',
-      profile:'images/Jay.jpeg'
-    },
-    { 
-      name: 'Keshav Jha', 
-      additionalInfo: 'Additional info for Keshav.',
-      skills: ['HTML/CSS', 'React.js','Bootstrap'],
-      designation: 'Frontend Developer',
-      profile:'images/keshav.jpeg'
-    },
+    }
+    // { 
+    //   name: 'Yuvraj Singh Lamba', 
+    //   additionalInfo: 'Additional info for Yuvraj.',
+    //   skills: ['HTML/CSS', 'React.js','Bootstrap'],
+    //   designation: 'Frontend Developer',
+    //   profile:'images/Yuvraj.jpeg'
+    // },
+    // { 
+    //   name: 'Jay Mehta', 
+    //   additionalInfo: 'Additional info for Jay.',
+    //   skills: ['HTML/CSS', 'React.js','Bootstrap'],
+    //   designation: 'Frontend Developer',
+    //   profile:'images/Jay.jpeg'
+    // },
+    // { 
+    //   name: 'Keshav Jha', 
+    //   additionalInfo: 'Additional info for Keshav.',
+    //   skills: ['HTML/CSS', 'React.js','Bootstrap'],
+    //   designation: 'Frontend Developer',
+    //   profile:'images/keshav.jpeg'
+    // },
   ];
   
   export default userData;
